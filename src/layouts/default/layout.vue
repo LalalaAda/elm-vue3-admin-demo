a50b5e77fb124ff58f1878414f9a2e0430c86fde
tzd001 <tuzhida2012@outlook.com>
2022-08-04 23:46:45 +0800

    feat: 添加路由及layout预填充文件

---
+++ src/layouts/default/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-layout>
+    <p>layout页</p>
+  </el-layout>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Layout',
+  setup() {
+    return {}
+  }
+})
+</script>
